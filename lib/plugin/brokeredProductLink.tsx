fea8446f8cc679405eeb52611c5ddacfb3ccf849
Andrew Wittrock <andy@wittrock.us>
2025-10-13 22:24:49 -0500

    Fix brokered product type

--- lib/plugin/brokeredProductLink.tsx
+++ lib/plugin/brokeredProductLink.tsx
@@ -1,7 +1,8 @@
+import { IBrokerableProduct } from "@brokered-products-plugin-shared/product/types";
 import { BrokeredProductButton } from "@brokered-products-plugin/components/BrokeredProductButton";
 import { IAddToCartPluginProps } from "@store/lib/plugin/slots";
 
 export const brokeredProductLink = {
-    filter: ({product}:IAddToCartPluginProps) => !!product.brokerageProductId,
+    filter: ({product}:IAddToCartPluginProps) => !!(product as IBrokerableProduct).brokerageProductId,
     plugin: ({product}:IAddToCartPluginProps) => <BrokeredProductButton product={product} />,
 }
